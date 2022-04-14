7b8ce3842762b43e3b95f27c35b97e12cbbf7758
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-14 15:53:01 -0700

    Add draw feature to grid

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
+const container = document.getElementById('grid');
+let drawing = false;
 //Used to create a single square in the grid
 function square(...id){
     const div = document.createElement('div');
     const row = document.getElementById(id[1]);
-    div.setAttribute('id', id[0]);
-    div.setAttribute('class', "square");
+    div.setAttribute('class', id[0]);
+    div.classList.add('square');
     div.style.width = "16px";
     div.style.height = "16px";
     row.appendChild(div);
@@ -12,8 +14,7 @@ function square(...id){
 //Used to create singular row of arbitrary size
 function row(length, id){
     const row = document.createElement('div');
-    const grid = document.getElementById('grid');
-    grid.appendChild(row);
+    container.appendChild(row);
     row.setAttribute('class', 'row');
     row.setAttribute('id', id);
     for(let i = 1; i<=length; i++){
@@ -27,4 +28,40 @@ function grid(length){
     }
 }
 
-grid(16);
+function drawFunction(event){
+    console.log(event.type);
+    console.log(drawing);
+    if(drawing){
+        this.style.backgroundColor = "black";
+    }    
+}
+//Checks to see if you are drawing or not and updates the global variable drawing
+function isDrawing(){    
+    container.addEventListener("mouseleave", function(e){
+        drawing = false;
+    });
+    container.addEventListener("mousedown", function(e){
+        drawing = true;
+    });
+    container.addEventListener("mouseup", function(e){
+        drawing = false;
+    });
+    
+}
+//The function that builds and draws the whole program
+function draw(){  
+    grid(16);
+    isDrawing();
+
+    boxes = document.querySelectorAll(".square");
+
+    boxes.forEach((box)=> {
+        box.addEventListener("mousemove", drawFunction);
+        box.addEventListener("mousedown", (e) =>{
+            box.style.backgroundColor = "black";
+        });
+    });
+
+}
+
+draw();
