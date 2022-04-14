54f8526157f2d0b1b6a4fbfba07becbe0e132964
Nicolas Rodriguez <94021624+NicRodri@users.noreply.github.com>
2022-04-14 11:26:37 -0700

    Create 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,28 +1,30 @@
 //Used to create a single square in the grid
-function square(id){
+function square(...id){
     const div = document.createElement('div');
-    const row = document.querySelector('.row');
-    div.setAttribute('id', id);
+    const row = document.getElementById(id[1]);
+    div.setAttribute('id', id[0]);
     div.setAttribute('class', "square");
     div.style.width = "16px";
     div.style.height = "16px";
     row.appendChild(div);
+    
 }
 //Used to create singular row of arbitrary size
-function row(length){
+function row(length, id){
     const row = document.createElement('div');
     const grid = document.getElementById('grid');
     grid.appendChild(row);
     row.setAttribute('class', 'row');
+    row.setAttribute('id', id);
     for(let i = 1; i<=length; i++){
-        square(String(i));
+        square(String(i), id);
     }
 }
 //Used to create grid of arbitrary size
 function grid(length){
-
+    for(let i=1; i<=length; i++){
+        row(length, "row" + String(i));
+    }
 }
 
-row(16);
-//row(10);
-//row(10);
+grid(16);
